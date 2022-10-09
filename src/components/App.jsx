1135cf113c041ab50b29ed4df26df4c59af462ab
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-10-09 12:07:25 +0300

    task 1-4 without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { Profile } from "./profile/Profile";
 import { Statistics } from "./statistics/Statistics";
+import { FriendList } from "./friends/FriendList";
+import { TransactionHistory } from "./transactions/TransactionHistory";
 import user from './profile/user.json'
 import data from './statistics/data.json'
+import friends from './friends/friends.json'
+import transactions from './transactions/transactions.json'
 
 export const App = () => {
   return (
@@ -12,6 +16,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats} />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/friends/FriendList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import { FriendListItem } from './FriendListItem'
+
+
+export const FriendList = ({friends}) => {
+    return <ul className="friend-list">
+        {friends.map(({ avatar, name, isOnline, id }) =>
+            <FriendListItem key={id} avatar={avatar}
+        name={name}
+        isOnline={isOnline}/>)}
+    </ul> 
+}
+
+FriendList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
